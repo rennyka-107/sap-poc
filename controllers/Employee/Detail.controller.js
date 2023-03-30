f40267743c19f45a8e931cf605889d8a0db65f4c
ANHNX4 <anh833905@gmail.com>
2023-03-30 09:36:32 +0700

    Employee tab details

--- controllers/Employee/Detail.controller.js
+++ controllers/Employee/Detail.controller.js
@@ -4,32 +4,42 @@ sap.ui.define(
     return BaseController.extend("sap-app.controllers.Employee.Detail", {
       onInit: function () {
         this._oObjectPage = this.getView().byId("ObjectPageLayout");
-        this._oObjectPage.attachEvent("subSectionVisibilityChange", this.onSectionVisibilityChanged, this);
+        this._oObjectPage.attachEvent(
+          "subSectionVisibilityChange",
+          this.onSectionVisibilityChanged,
+          this
+        );
       },
-      onAfterRendering: function() {
-        const {id} = this.getCurrentParams();
-        if(id) {
-          let model = new JSONModel({id});
+      onAfterRendering: function () {
+        const { id } = this.getCurrentParams();
+        if (id) {
+          let model = new JSONModel({ id });
           this.getView().setModel(model);
         }
       },
-      getCurrentParams: function(router = this.getOwnerComponent().getRouter()) {
+      getCurrentParams: function (
+        router = this.getOwnerComponent().getRouter()
+      ) {
         const currentHash = router.getHashChanger().getHash();
         const { arguments } = router.getRouteInfoByHash(currentHash);
         return arguments;
       },
-      onSectionVisibilityChanged: function(oEvent) {
+      onSectionVisibilityChanged: function (oEvent) {
         var oVisibleSubSections = oEvent.getParameter("visibleSubSections"),
           aSubSectionsIds = Object.keys(oVisibleSubSections);
-  
+
         if (aSubSectionsIds.length === 1) {
-          oVisibleSubSections[aSubSectionsIds[0]].addStyleClass("sapUxAPObjectPageSubSectionFitContainer");
+          oVisibleSubSections[aSubSectionsIds[0]].addStyleClass(
+            "sapUxAPObjectPageSubSectionFitContainer"
+          );
         } else {
-          aSubSectionsIds.forEach(function(sKey) {
-            oVisibleSubSections[sKey].removeStyleClass("sapUxAPObjectPageSubSectionFitContainer");
+          aSubSectionsIds.forEach(function (sKey) {
+            oVisibleSubSections[sKey].removeStyleClass(
+              "sapUxAPObjectPageSubSectionFitContainer"
+            );
           });
         }
-      }
+      },
     });
   }
 );
