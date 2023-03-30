039b1bdf1a7120cb4d74e85e76817105fc25ef6a
duonnh200 <duongnh@sphinxjsc.com>
2023-03-30 09:50:09 +0700

    feat: create form salary,switch radio

--- components/Employee/employment/BlockDetailPart3.js
+++ components/Employee/employment/BlockDetailPart3.js
@@ -1,25 +1,45 @@
-sap.ui.define(["sap/uxap/BlockBase"], function (BlockBase) {
-  "use strict";
+sap.ui.define(
+  ["sap/uxap/BlockBase", "sap/ui/model/json/JSONModel"],
+  function (BlockBase, JSONModel) {
+    "use strict";
 
-  var BlockEmpDetailPart1 = BlockBase.extend(
-    "sap-app.components.Employee.employment.BlockDetailPart3",
-    {
-      metadata: {
-        views: {
-          Collapsed: {
-            viewName: "sap-app.components.Employee.employment.BlockDetailPart3",
-            type: "XML",
-          },
-          Expanded: {
-            viewName: "sap-app.components.Employee.employment.BlockDetailPart3",
-            type: "XML",
+    var BlockEmpDetailPart1 = BlockBase.extend(
+      "sap-app.components.Employee.employment.BlockDetailPart3",
+      {
+        metadata: {
+          views: {
+            Collapsed: {
+              viewName:
+                "sap-app.components.Employee.employment.BlockDetailPart3",
+              type: "XML",
+            },
+            Expanded: {
+              viewName:
+                "sap-app.components.Employee.employment.BlockDetailPart3",
+              type: "XML",
+            },
           },
         },
-      },
-      handleClose: function (oEvent) {
-        console.log(oEvent);
-      },
-    }
-  );
-  return BlockEmpDetailPart1;
-});
+     
+        init: function () {
+          BlockBase.prototype.init.apply(this, arguments);
+          const model = new JSONModel({
+            number: 68688,
+            time: 112,
+            amount: 12,
+            salary: 10000,
+          });
+          this.setModel(model);
+        },
+
+        handleClose: function (oEvent) {
+          console.log(oEvent);
+        },
+        handleSubmit: function () {
+          console.log(this.getModel());
+        },
+      }
+    );
+    return BlockEmpDetailPart1;
+  }
+);
